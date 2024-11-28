9fe53251ef549a7209a15a28bedad26a4ecb9405
Jethro Mwantok <Jethromwantok@gmail.com>
2024-11-28 19:58:27 +0100

    Initial commit

---
+++ Learn.java
@@ -0,0 +1,14 @@
+public class Learn {
+    public static void main(String[] args) {
+        //final long thatNum = 2399999L;//Final used if you do not intend to change value
+        //int me;
+        //float mee;//used with f
+        //double meee;//used with d
+        //System.out.println("Hello World!");
+        //System.out.println("User " + thatNum + " is active");
+        //double fi = 4.23e8d;
+        //System.out.println(fi);
+        char myVar = 66;//This is using its ASCII value
+        System.out.println(myVar);
+    }
+}
