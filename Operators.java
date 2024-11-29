5eaf5ffd7ac8720fcdecf6487f11cadd7dc27cd1
Jethro Mwantok <Jethromwantok@gmail.com>
2024-11-29 11:49:28 +0100

    this

--- Operators.java
+++ Operators.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Operators {
+public class Operators {
+    public static void main(String[] args) {
+        /*
+        * x++ (Increases x by 1)
+        * x+=1 (Adds number on right to the left and stores it in the left)
+        * && - Java and
+        * || - java or
+        * | - java not*/
+    }
 }
