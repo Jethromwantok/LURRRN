3f44e64c07549aa5b75132a3a1e10f1b47b75f13
Jethro Mwantok <Jethromwantok@gmail.com>
2024-12-03 21:02:43 +0100

    Create ifStatements.java

---
+++ ifStatements.java
@@ -0,0 +1,17 @@
+public class ifStatements {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 18;
+        if (x > y) {
+            System.out.println("x is greater than y");
+
+        }
+        int time = 20;
+        if (time < 18) {
+            System.out.println("Good day.");
+        } else {
+            System.out.println("Good evening.");
+        }
+// Outputs "Good evening."
+    }
+}
